f5dc2949161be5e2ab8c3e03250861e2c5799f02
Sarah Mordelt <sarah.mordelt@hp.com>
2015-09-24 14:44:26 +0200

    UserService-changed

---
+++ meetmeserver/src/main/java/de/dhbw/meetme/rest/GPSService.java
@@ -0,0 +1,31 @@
+package de.dhbw.meetme.rest;
+
+import de.dhbw.meetme.database.dao.UserDao;
+import groovy.lang.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * Created by mordelt on 24.09.2015.
+ * This class handles all GPS-related interactions.
+ */
+@Path("api/gps")
+@Produces({"application/json"}) // mime type
+@Singleton
+public class GPSService {
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
+
+    @Inject
+    UserDao userDao;
+
+    @Path("/send")
+    @POST
+    public void sendGPS(){
+        //class that sends GPS and user data to database
+    }
+}
