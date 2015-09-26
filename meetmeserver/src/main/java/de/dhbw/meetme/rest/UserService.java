1444a80e59b351eb9252f1bdae2ba41f6502cd26
Aron Bussalb <aron.ben.bussalb@hpe.com>
2015-09-26 10:59:14 +0200

    Merge branch 'master' of https://github.com/DieAndroiden/meetme
    # Conflicts:
    #	README

--- meetmeserver/src/main/java/de/dhbw/meetme/rest/UserService.java
+++ meetmeserver/src/main/java/de/dhbw/meetme/rest/UserService.java
@@ -10,6 +10,7 @@ import sun.rmi.runtime.Log;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,7 +19,8 @@ import java.util.Map;
  *
  */
 @Path("/api/user")
-@Produces({"application/json"}) // mime type
+@Produces(MediaType.APPLICATION_JSON)
+//@Produces({"application/json"}) // mime type
 @Singleton
 public class UserService {
   private static final Logger log = LoggerFactory.getLogger(UserService.class);
@@ -49,17 +51,28 @@ public class UserService {
 
   @Path("/save")
   @POST
-  public void save(@FormParam("regEmail") String regEmail, @FormParam("userName") String userName, @FormParam("registerPassword") String registerPassword, @PathParam("nationality") String nationality)
-
+  public String save(@FormParam("regEmail") String regEmail, @FormParam("userName") String userName, @FormParam("registerPassword") String registerPassword, @PathParam("nationality") String nationality, @PathParam("description") String description){
+    // prüfen ob alle Felder ausgefüllt sind...sollte eig schon bei dem Client geprüft worden sein
     if ( registerPassword == "" | regEmail == "" | userName == "" ){
       log.debug(userName + "tried to register but not all forms were filled");
-      // Meldung an Browser bzw. App das nicht alles ausgefüllt worden ist
-      // Registrierung abbrechen
+      return "Bitte fülle alle Felder aus";
     }
-
+    // prüfen ob der userName schon vergebe ist
+    if (userDao.existCeckName(userName) == true)
+    {
+      log.debug(userName +"tried ro register but the userName is already in use");
+      return "Leider existiert dein Username breits, suche dir bitte einen neuen aus";
+    }
+    //prüfen ob die Mail schon verwendent worden ist.
+    if (userDao.existCeckMail(regEmail) == true)
+    {
+      log.debug(userName +"tried ro register but the regMail is already in use");
+      return "Du hast mit der Mail bereits einen Account erstellt";
+    }
     //userDao.persist(regEmail, userName, registerPassword);
     //userDao.persist(user);
     //log.debug("Save user " + user);
+    return  "Hallo " + userName +" du wurdest erfolgereich registriert! :) deine Mail: " +  regEmail;
   }
 
 }
